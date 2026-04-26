e1d7e5ef5421930d021d8d8d67c64922136340f6
Rottmayer <rottmayer1989@gmail.com>
2026-04-26 18:54:08 +0400

    init prject

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+    <style>
+        [x-cloak] {
+            display: none;
+        }
+    </style>
+
+
+</head>
+
+<body>
+
+
+
+    @livewireScripts
+</body>
+
+</html>
